f9907413a1b50ab24ac60d1ae7de47d5eb116c0b
ShriVenkateshBabu <shrivenkatesh679@gmail.com>
2024-09-20 20:03:29 +0530

    second

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+const Register = () => {
+  let [regsiterInfo, setRegsiterInfo] = useState({
+    fullName: "",
+    userName: "",
+    password: "",
+    role: "",
+  });
 
-let Register = () => {
+  
+  function handleChange(eve) {
+    setRegsiterInfo((preVal) => ({
+      ...preVal,
+      [eve.target.id]: eve.target.value,
+    }));
+  }
+
+  async function handleSubmit() {
+    console.log(regsiterInfo);
+    setRegsiterInfo({
+      fullName: "",
+      userName: "",
+      password: "",
+      role: "Basic",
+    });
+    
+  }
   return (
     <div className="grid grid-cols-12 m-10 p-5 gap-5 border border-blue-900">
       <div className="full-name col-span-8 col-start-2 border border-blue-500">
@@ -8,8 +32,8 @@ let Register = () => {
           type="text"
           id="fullName"
           required
-        //   onChange={handleChange}
-        //   value={regsiterInfo.fullName}
+          onChange={handleChange}
+          value={regsiterInfo.fullName}
           placeholder="Enter your name"
         />
       </div>
@@ -18,8 +42,8 @@ let Register = () => {
           type="text"
           id="userName"
           required
-        //   onChange={handleChange}
-        //   value={regsiterInfo.userName}
+          onChange={handleChange}
+          value={regsiterInfo.userName}
           placeholder="Enter user name"
         />
       </div>
@@ -28,31 +52,34 @@ let Register = () => {
           type="text"
           id="password"
           required
-        //   onChange={handleChange}
-        //   value={regsiterInfo.password}
+          onChange={handleChange}
+          value={regsiterInfo.password}
           placeholder="Enter your password"
         />
       </div>
 
-      {/* /* <div className="password col-span-8 col-start-2 border border-blue-500">
+      <div className="password col-span-8 col-start-2 border border-blue-500">
         <select onChange={handleChange} id="role" value={regsiterInfo.role}>
+          <optgroup label ="role">
           <option>Basic</option>
           <option>Admin</option>
+          </optgroup>
         </select>
       </div>
 
       <div className="register-action-btn col-span-8 col-start-2">
         <button
           onClick={handleSubmit}
-          className="m-2 p-2 border border-yellow rounded-2xl bg-slate-400" >
+          className="m-2 p-2 border border-yellow rounded-2xl bg-slate-400"
+        >
           Register
         </button>
         <button className="m-2 p-2 border border-yellow rounded-2xl bg-slate-400">
           Cancel
         </button>
-      </div> */}
-    </div> 
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import "./index.css"
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import DataProvider from './context/DataProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-     <Routes>
+   <DataProvider>
+   <Routes>
        <Route path ="*" element= {<App/>}/>
       </Routes> 
+   </DataProvider>
     </BrowserRouter>
   </StrictMode>
 );
